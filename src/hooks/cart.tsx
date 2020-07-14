f66e2a0944acdf40a23f21a7e966adee4f832c18
Djanilson Martins <djanlm@hotmail.com>
2020-07-14 09:20:50 -0300

    desafio_finalizado_todos_testes_passaram

--- src/hooks/cart.tsx
+++ src/hooks/cart.tsx
@@ -31,22 +31,78 @@ const CartProvider: React.FC = ({ children }) => {
   useEffect(() => {
     async function loadProducts(): Promise<void> {
       // TODO LOAD ITEMS FROM ASYNC STORAGE
+      const storedProducts = await AsyncStorage.getItem(
+        '@GoMarketPlace:products',
+      );
+      if (storedProducts) {
+        setProducts(JSON.parse(storedProducts));
+      }
     }
 
     loadProducts();
   }, []);
 
-  const addToCart = useCallback(async product => {
-    // TODO ADD A NEW ITEM TO THE CART
-  }, []);
+  const increment = useCallback(
+    async id => {
+      const updatedProducts = products.map(item => {
+        if (item.id === id) {
+          return { ...item, quantity: item.quantity + 1 };
+          // item.quantity += 1;
+        }
+        return item;
+      });
+      setProducts(updatedProducts);
+      await AsyncStorage.setItem(
+        '@GoMarketPlace:products',
+        JSON.stringify(updatedProducts),
+      );
+    },
+    [products],
+  );
 
-  const increment = useCallback(async id => {
-    // TODO INCREMENTS A PRODUCT QUANTITY IN THE CART
-  }, []);
+  const addToCart = useCallback(
+    async product => {
+      // TODO ADD A NEW ITEM TO THE CART
+      const productAlreadyAdded = products.find(item => item.id === product.id);
+      // caso o produto adicionado já esteja no carrinho, ele não será adicionado, mas seu quantity será incrementado
 
-  const decrement = useCallback(async id => {
-    // TODO DECREMENTS A PRODUCT QUANTITY IN THE CART
-  }, []);
+      if (productAlreadyAdded) {
+        const updatedProduct = {
+          ...productAlreadyAdded,
+          quantity: productAlreadyAdded.quantity + 1,
+        }; // spread as outras variaveis e atualiza o quantity
+        setProducts([...products, updatedProduct]);
+      } else {
+        const newProduct = { ...product, quantity: 1 }; // adiciona a variavel quantity ao novo produto
+        setProducts([...products, newProduct]);
+        await AsyncStorage.setItem(
+          '@GoMarketPlace:products',
+          JSON.stringify(products),
+        );
+      }
+    },
+    [products],
+  );
+
+  const decrement = useCallback(
+    async id => {
+      const updatedProducts = products.map(item => {
+        if (item.id === id) {
+          if (item.quantity > 1) {
+            return { ...item, quantity: item.quantity - 1 };
+          }
+          // item.quantity += 1;
+        }
+        return item;
+      });
+      setProducts(updatedProducts);
+      await AsyncStorage.setItem(
+        '@GoMarketPlace:products',
+        JSON.stringify(updatedProducts),
+      );
+    },
+    [products],
+  );
 
   const value = React.useMemo(
     () => ({ addToCart, increment, decrement, products }),
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  // baseURL: 'http://10.0.2.2:3333', // android studio
+  // baseURL: 'http://10.0.3.2:3333', // genymotion
+  baseURL: 'http://localhost:3333', // ios
+  // baseURL: 'http://ip_da_rede:3333', // via usb
 });
 
 export default api;
